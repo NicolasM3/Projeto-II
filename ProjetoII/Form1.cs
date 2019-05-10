56b92029fa8aac5207bd59bfbf981a20fed84071
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2019-05-10 08:46:24 -0300

    Adicionado design do formulário

--- ProjetoII/Form1.cs
+++ ProjetoII/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ProjetoII
 {
-	public partial class Form1 : Form
+	public partial class FormForca : Form
 	{
-		public Form1()
+		public FormForca()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			bool abriu = false;
+			while (!abriu)
+			{
+				if (dlgAbrir.ShowDialog() == DialogResult.OK)
+				{
+					abriu = true;
+					LerArquivo(dlgAbrir.FileName);
+					txtNome.Focus();
+				}
+			}
+		}
+
+		private void LerArquivo(string arquivo)
+		{
+			var reader = new StreamReader(arquivo);
+			while (!reader.EndOfStream)
+			{
+				string linhaLida = reader.ReadLine();
+				string palavra = linhaLida.Substring(0, 15);
+				string dica = linhaLida.Substring(16);
+			}
 		}
 	}
 }
